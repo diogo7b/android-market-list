1d98c43c846c33f29619fa95079cfeeaf745fa25
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2024-07-03 16:47:10 -0300

    implemented Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlinKsp)
 }
 
 android {
@@ -33,9 +34,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    //room implements
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
