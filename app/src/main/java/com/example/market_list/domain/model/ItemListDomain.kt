eb6c82716619242cde72dac0b7c6f3624ac6efbf
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2024-07-04 16:30:49 -0300

    changed type of propertie unitPrice and totalPrice from Int to Double in ItemListDomain

--- app/src/main/java/com/example/market_list/domain/model/ItemListDomain.kt
+++ app/src/main/java/com/example/market_list/domain/model/ItemListDomain.kt
@@ -3,7 +3,7 @@ package com.example.market_list.domain.model
 data class ItemListDomain(
     val id: Int,
     val name: String,
-    val unitPrice: String,
+    val unitPrice: Double,
     val amount: Int,
-    val totalPrice: String
+    val totalPrice: Double
 )
