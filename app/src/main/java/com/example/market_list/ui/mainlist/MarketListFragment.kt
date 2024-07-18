40ee1209c8f9770d82df357338c2d06457bda45a
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2024-07-18 10:35:22 -0300

    feat: new MarketListFragment

---
+++ app/src/main/java/com/example/market_list/ui/mainlist/MarketListFragment.kt
@@ -0,0 +1,10 @@
+package com.example.market_list.ui.mainlist
+
+import androidx.fragment.app.Fragment
+import com.example.market_list.databinding.FragmentMarketListBinding
+
+class MarketListFragment : Fragment() {
+    private var _binding: FragmentMarketListBinding? = null
+    private val binding get() = _binding!!
+
+}
