659c0401884aea5e37ad9464cacf07c0cffa5c5a
Diogo Borges <54003586+diogo7b@users.noreply.github.com>
2024-08-26 11:12:50 -0300

    feat: moved to your own products list use cases directory

--- app/src/main/java/com/example/market_list/domain/useCases/InsertProductUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.market_list.domain.useCases
-
-import com.example.market_list.domain.model.ItemListDomain
-import com.example.market_list.domain.repository.MarketListRepository
-
-class InsertProductUseCase(private val repository: MarketListRepository) {
-    suspend operator fun invoke(item: ItemListDomain) = repository.insertProduct(item)
-}
